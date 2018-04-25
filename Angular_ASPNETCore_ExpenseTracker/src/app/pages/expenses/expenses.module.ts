27d7883916a002cc548c49cda6eb6ebaeaad7648
DESKTOP\User <ab.1303@gmail.com>
2018-04-25 22:26:09 +1000

    Expense Sheet -- in Progress
    ... webpack failure

--- Angular_ASPNETCore_ExpenseTracker/src/app/pages/expenses/expenses.module.ts
+++ Angular_ASPNETCore_ExpenseTracker/src/app/pages/expenses/expenses.module.ts
@@ -2,16 +2,22 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+
 import { TrackByService } from "../../core/trackby.service";
 import { SharedModule } from "../../shared/shared.module";
 
 import { ExpensesComponent } from "./expenses.component";
 import { ExpensesRoutes } from "./expenses.routes";
 import { ExpenseSheetComponent } from "./expense-sheet/expense-sheet.component";
+import { ExpenseSheetService } from './expense-sheet/expense-sheet.service';
 
 @NgModule({
   declarations: [ExpensesComponent, ExpenseSheetComponent],
-  imports: [CommonModule, SharedModule.forRoot(), ExpensesRoutes],
-  providers: [TrackByService]
+  imports: [
+    CommonModule, 
+    NgxDatatableModule, 
+    SharedModule.forRoot(), ExpensesRoutes],
+  providers: [TrackByService, ExpenseSheetService]
 })
 export class ExpensesModule {}
