58c0c3d60129be4a7a951ed5b9f7e2a6ed006f74
DESKTOP\User <ab.1303@gmail.com>
2018-08-13 23:12:41 +1000

    Committed broken build

--- Angular_ASPNETCore_ExpenseTracker/src/app/pages/reports/expense-category/expense-category.component.ts
+++ Angular_ASPNETCore_ExpenseTracker/src/app/pages/reports/expense-category/expense-category.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { NgDateRangePickerOptions } from 'ng-daterangepicker';
 
+import { IExpenseCategory } from './expense-category';
+import { ExpenseCategoryReportService } from './expense-category-report.service';
+
 @Component({
   selector: 'app-expense-category',
   templateUrl: './expense-category.component.html',
   styleUrls: ['./expense-category.component.scss']
 })
 export class ExpenseCategoryComponent implements OnInit {
+  expenseCategories: IExpenseCategory[] = [];
+  rows = [];
   options: NgDateRangePickerOptions;
 
-  constructor() { }
+  constructor(
+    private expenseCategoryReportService: ExpenseCategoryReportService
+  ) { }
 
   ngOnInit() {
     this.options = {
@@ -20,7 +27,13 @@ export class ExpenseCategoryComponent implements OnInit {
 	  dateFormat: 'yMd',
 	  outputFormat: 'DD/MM/YYYY',
 	  startOfWeek: 1
-	};
+  };
+  
+  this.expenseCategoryReportService.getExpenseCategories().subscribe(data => {
+    // this.transactions = data.transactions;
+    this.rows = data.expenseCategories;
+  });
+
   }
 
 }
