b8cf75ea044aa07dce05659cb6e3fa2703907b89
Abdul <ab.1303@gmail.com>
2018-08-24 17:25:39 +1000

    AE-6 - [In Progress] Search Facility in Expense Sheet - server side

--- Angular_ASPNETCore_ExpenseTracker/src/app/pages/expenses/expense-register/expense-register.component.ts
+++ Angular_ASPNETCore_ExpenseTracker/src/app/pages/expenses/expense-register/expense-register.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, HostListener } from "@angular/core";
 import { ITransaction } from "./transaction";
 import { ExpenseRegisterService } from "./expense-register.service";
 import { TrackByService } from "../../../core/trackby.service";
+import { MdSidenav } from "@angular/material";
+import { ConfigService } from "../../../shared/services/config/config.service";
 
 @Component({
-  selector: "expense-register",
-  templateUrl: "./expense-register.component.html",
-  styleUrls: ["./expense-register.component.scss"]
+	selector: "expense-register",
+	templateUrl: "./expense-register.component.html",
+	styleUrls: ["./expense-register.component.scss"]
 })
 export class ExpenseRegisterComponent implements OnInit {
-  transactions: ITransaction[] = [];
-  rows = [];
+	transactions: ITransaction[] = [];
+	rows = [];
 	selected = [];
 	temp = [];
 	searchValue: string = null;
@@ -18,19 +20,35 @@ export class ExpenseRegisterComponent implements OnInit {
 	isToolbarActive: boolean = false;
 	itemsSelected: string = "";
 	itemCount: number = 0;
-  constructor(
-    private expenseRegisterService: ExpenseRegisterService,
-    public trackby: TrackByService
-  ) {}
+	@ViewChild("rightSidenav2") rightSidenav2: MdSidenav;
+	navMode = "side";
+	constructor(
+		private expenseRegisterService: ExpenseRegisterService,
+		public config: ConfigService,
+		public trackby: TrackByService
+	) { }
 
-  ngOnInit() {
-    this.expenseRegisterService.getTransactions().subscribe(data => {
-      // this.transactions = data.transactions;
-      this.rows = data.individualTransactions;
-    });
-  }
+	ngOnInit() {
+		this.expenseRegisterService.getTransactions().subscribe(data => {
+			// this.transactions = data.transactions;
+			this.rows = data.individualTransactions;
+		});
+	}
 
-  onSelect({ selected }) {
+	
+	@HostListener("window:resize", ["$event"])
+	onResize(event) {
+		if (event.target.innerWidth < this.config.breakpoint.desktopLG) {
+			this.navMode = "over";
+			this.rightSidenav2.close();
+		}
+		if (event.target.innerWidth > this.config.breakpoint.desktopLG) {
+			this.navMode = "side";
+			this.rightSidenav2.open();
+		}
+	}
+
+	onSelect({ selected }) {
 		//console.log("Select Event", selected, this.selected);
 		//console.log(this.selected.length);
 		this.selected.splice(0, this.selected.length);
@@ -46,9 +64,9 @@ export class ExpenseRegisterComponent implements OnInit {
 		} else {
 			this.isToolbarActive = false;
 		}
-  }
-  
-  triggerClose(event) {
+	}
+
+	triggerClose(event) {
 		this.rows = this.temp;
 		this.searchValue = "";
 		this.isSearchActive = !this.isSearchActive;
