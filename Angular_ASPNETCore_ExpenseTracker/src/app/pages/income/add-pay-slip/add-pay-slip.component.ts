0015ed461ba29eb36156cb8f3cd3ab0dbab351cf
Abdul <ab.1303@gmail.com>
2018-10-08 22:27:56 +1100

    AE-23 [In-Progress] Create Records for Salary Slips

--- Angular_ASPNETCore_ExpenseTracker/src/app/pages/income/add-pay-slip/add-pay-slip.component.ts
+++ Angular_ASPNETCore_ExpenseTracker/src/app/pages/income/add-pay-slip/add-pay-slip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pay-slip',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPaySlipComponent implements OnInit {
 
+  addPaySlipForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.addPaySlipForm = new FormGroup({
+      frequency: new FormControl('', Validators.required),
+      periodStart: new FormControl('', Validators.required),
+      periodEnd: new FormControl('', Validators.required),
+      totalEarnings: new FormControl('', Validators.required),
+      netPay: new FormControl('', Validators.required),
+      tax: new FormControl({ value: '', disabled: true, }),
+      taxPct: new FormControl({ value: '', disabled: true, }),
+      superAnnuation: new FormControl('', Validators.required),
+      superAnnuationPct: new FormControl({ value: '', disabled: true, },),
+    });
+  }
+
+  calculateTaxPct() {
+    const netPayCtrl: AbstractControl = this.addPaySlipForm.get('netPay').value;
+    const totalEarningsCtrl: AbstractControl = this.addPaySlipForm.get('totalEarnings').value;
+
+    console.log(netPayCtrl);
+    console.log(totalEarningsCtrl);
+
   }
 
 }
