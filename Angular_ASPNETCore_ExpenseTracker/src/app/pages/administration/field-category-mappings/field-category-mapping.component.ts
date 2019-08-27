e2bdcd77a0cbabff5d7e95ebd33236648c19c0cd
DESKTOP\User <ab.1303@gmail.com>
2019-08-27 23:42:11 +1000

    AE-54: Refactoring to presentation components

--- Angular_ASPNETCore_ExpenseTracker/src/app/pages/administration/field-category-mappings/field-category-mapping.component.ts
+++ Angular_ASPNETCore_ExpenseTracker/src/app/pages/administration/field-category-mappings/field-category-mapping.component.ts
@@ -1,23 +1,18 @@
-import { Component, AfterViewInit, ViewChild, OnDestroy, ViewChildren, QueryList } from "@angular/core";
-import { BehaviorSubject, Subscription, Observable } from "rxjs/Rx";
+import { Component, OnDestroy, ViewChildren, QueryList } from "@angular/core";
+import { Subscription, Observable } from "rxjs/Rx";
 import { v4 as uuid } from "uuid";
 
 import { Item } from "./item.model";
-import { FilterTextComponent } from "../../../shared/components/filter-text/filter-text.component";
-import { tap } from "rxjs/operators/tap";
 import { ToggleCardItemComponent } from "../../../shared/components/toggle-card-item/toggle-card-item.component";
-import { from, combineLatest, of, interval, Subject } from "rxjs";
-import { mergeAll, map, mergeMap, switchAll, merge, switchMap } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import { ExpenseCategoryService } from "../expense-category/expense-category.service";
-import { ExpenseCategoryApiResponse } from "../expense-category/expense-category";
-import { publishReplay } from "rxjs-compat/operator/publishReplay";
 
 @Component({
     selector: "field-category-mapping",
     templateUrl: "./field-category-mapping.component.html",
     styleUrls: ["./field-category-mapping.component.scss"]
 })
-export class FieldCategoryMappingComponent implements AfterViewInit, OnDestroy {
+export class FieldCategoryMappingComponent implements OnDestroy {
     private sourceCardItemsArray: Item[] = [
         {
             id: uuid(),
@@ -40,12 +35,8 @@ export class FieldCategoryMappingComponent implements AfterViewInit, OnDestroy {
 
     editableText: string;
     private subscriptions: { [key: string]: Subscription } = {};
-    private sourceClicksSubscriptions: { [key: string]: BehaviorSubject<Observable<Item>> } = {};
-    private categoryClicksSubscriptions: { [key: string]: BehaviorSubject<Observable<Item>> } = {};
 
     // View Refs
-    @ViewChild("filterSources") filterSources: FilterTextComponent;
-    @ViewChildren("source", { read: ToggleCardItemComponent }) sourceCardItems!: QueryList<ToggleCardItemComponent>;
     @ViewChildren("destination", { read: ToggleCardItemComponent }) categoryCardItems!: QueryList<ToggleCardItemComponent>;
 
     expenseCategories$: Observable<Item[]> = this.expenseCategoryService.getExpenseCategories().pipe(
@@ -60,13 +51,6 @@ export class FieldCategoryMappingComponent implements AfterViewInit, OnDestroy {
         )
     );
 
-    sourceCardItemsArray$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(this.sourceCardItemsArray);
-    sourceCardItemsUpdate$: Subject<any> = new Subject<any>();
-    sourceFilter$: Observable<string>;
-
-    categoryCardItemsArray$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>([]);
-    categoryCardItemsUpdate$: Subject<any> = new Subject<any>();
-    categoryFilter$: Observable<string>;
     vm$: Observable<any>;
 
     /**
@@ -80,111 +64,11 @@ export class FieldCategoryMappingComponent implements AfterViewInit, OnDestroy {
 
     ngOnInit(): void {
         this.expenseCategories$.subscribe(categories => {
-            this.categoryCardItemsArray = categories;
-            this.categoryCardItemsArray$.next(categories);
+            this.categoryCardItemsArray = [...categories];
         });
-    }
-    ngAfterViewInit() {
-        this.sourceCardItems.changes.subscribe(() => {
-            setTimeout(() => {
-                this.sourceCardItems.forEach(si => {
-                    if (!this.sourceClicksSubscriptions[si.item.id]) {
-                        this.sourceClicksSubscriptions[si.item.id] = new BehaviorSubject(si.clicks$);
-                    } else {
-                        this.sourceClicksSubscriptions[si.item.id].next(si.clicks$);
-                    }
-                });
-                this.sourceCardItemsUpdate$.next();
-            }, 0);
-        });
-
-        this.categoryCardItems.changes.subscribe(() => {
-            setTimeout(() => {
-                this.categoryCardItems.forEach(ci => {
-                    if (!this.categoryClicksSubscriptions[ci.item.id]) {
-                        this.categoryClicksSubscriptions[ci.item.id] = new BehaviorSubject(ci.clicks$);
-                    } else {
-                        this.categoryClicksSubscriptions[ci.item.id].next(ci.clicks$);
-                    }
-                });
-                this.categoryCardItemsUpdate$.next();
-            }, 0);
-        });
-
-        this.sourceCardItemsUpdate$.subscribe(() => {
-            (<any>Object).entries(this.sourceClicksSubscriptions).forEach(([key, subjectClick$]) => {
-                if (subjectClick$.observers.length === 0) {
-                    this.subscriptions[`${key}`] = subjectClick$.pipe(switchAll()).subscribe(newItem => {
-                        console.log(`source Item clicked:`, newItem);
-                        this.sourceCardItemsArray.map(si => {
-                            if (si.id === newItem.id) {
-                                si.isActive = !si.isActive;
-                                return si;
-                            }
-                            si.isActive = false;
-                            return si;
-                        });
-                    });
-                }
-            });
-        });
-
-        this.categoryCardItemsUpdate$.subscribe(() => {
-            (<any>Object).entries(this.categoryClicksSubscriptions).forEach(([key, subjectClick$]) => {
-                if (subjectClick$.observers.length === 0) {
-                    this.subscriptions[`${key}`] = subjectClick$.pipe(switchAll()).subscribe(newItem => {
-                        console.log(`category Item clicked:`, newItem);
-                        this.categoryCardItemsArray.map(ci => {
-                            if (ci.id === newItem.id) {
-                                ci.isActive = !ci.isActive;
-                                return ci;
-                            }
-                            ci.isActive = false;
-                            return ci;
-                        });
-                    });
-                }
-            });
-        });
-
-        this.subscriptions.sourceFilterSubscription =
-            this.filterSources &&
-            this.filterSources.filterText$
-                .pipe(
-                    tap(filterText =>
-                        this.sourceCardItemsArray$.next(
-                            this.sourceCardItemsArray.filter(src => {
-                                return src.text.startsWith(filterText);
-                            })
-                        )
-                    )
-                )
-                .subscribe();
     }
 
     saveEditable(value) {
         //call to http service
-    }
-    onAddSourceItem(data) {
-        if (!data) return;
-
-        if (!this.sourceCardItemsArray.find(src => src.text === data)) {
-            this.sourceCardItemsArray = [
-                ...this.sourceCardItemsArray,
-                {
-                    id: uuid(),
-                    text: data,
-                    isActive: false
-                }
-            ];
-        }
-        this.sourceCardItemsArray$.next(this.sourceCardItemsArray);
-    }
-
-    onRemoveItem(data) {
-        console.log(data);
-        if (!data) return;
-        this.sourceCardItemsArray = this.sourceCardItemsArray.filter(s => s.text !== data);
-        this.sourceCardItemsArray$.next(this.sourceCardItemsArray);
     }
 }
--- Angular_ASPNETCore_ExpenseTracker/src/app/shared/components/card-item-list/card-item-list.component.ts
+++ Angular_ASPNETCore_ExpenseTracker/src/app/shared/components/card-item-list/card-item-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, EventEmitter, Output, ElementRef, ViewChild, ViewChildren, QueryList, AfterViewInit } from "@angular/core";
+import { Component, Input, ElementRef, ViewChild, ViewChildren, QueryList, AfterViewInit, OnInit, SimpleChanges } from "@angular/core";
 import { Observable, Subscription, BehaviorSubject, Subject } from "rxjs";
-import { tap, map, share, switchAll } from "rxjs/operators";
+import { tap, switchAll } from "rxjs/operators";
 import { Item } from "../../../pages/administration/field-category-mappings/item.model";
 import { FilterTextComponent } from "../filter-text/filter-text.component";
 import { ToggleCardItemComponent } from "../toggle-card-item/toggle-card-item.component";
@@ -9,66 +9,44 @@ import { v4 as uuid } from "uuid";
 @Component({
     selector: "card-item-list",
     styleUrls: ["./card-item-list.component.scss"],
-    template: `
-        <app-filter-text #filterItems [normalLabel]="'Source'" [filterLabel]="'Filter Source'"></app-filter-text>
-
-        <inline-editor [label]="'Source Category'" (onBlur)="onAddItem($event)" (onEnter)="onAddItem($event)">
-            <inline-normal>
-                <div class="add-item">
-                    <mat-icon>add</mat-icon>
-                </div>
-            </inline-normal>
-            <inline-edit>
-                <div class="input-item">
-                    <input inlineEditInputRef type="text" />
-                </div>
-            </inline-edit>
-        </inline-editor>
-        <div class="body">
-            <div *ngFor="let cardItem of cardItemsArray$ | async">
-                <toggle-card-item (onRemove)="onRemoveItem($event)" [item]="cardItem" [activeColor]="'#5ed07d'"></toggle-card-item>
-            </div>
-        </div>
-    `
+    templateUrl: "./card-item-list.component.html"
 })
-export class CardItemListComponent implements AfterViewInit {
-    @Input() label:string;
-    @Input() cardItemsArray: Array<Item>;
-    // TODO: Take as input
-    // private cardItemsArray: Item[] = [
-    //     {
-    //         id: uuid(),
-    //         text: "Test1",
-    //         isActive: false
-    //     },
-    //     {
-    //         id: uuid(),
-    //         text: "Test2",
-    //         isActive: false
-    //     },
-    //     {
-    //         id: uuid(),
-    //         text: "Test3",
-    //         isActive: false
-    //     }
-    // ];
-
+export class CardItemListComponent implements OnInit, AfterViewInit {
+    @Input() label: string;
+    private filterLabel: string;
+    private editorLabel: string;
     private subscriptions: { [key: string]: Subscription } = {};
-    private sourceClicksSubscriptions: { [key: string]: BehaviorSubject<Observable<Item>> } = {};
+    private clicksSubscriptions: { [key: string]: BehaviorSubject<Observable<Item>> } = {};
 
     // View Refs
     @ViewChild("filterItems") filterItems: FilterTextComponent;
     @ViewChildren(ToggleCardItemComponent) cardItems!: QueryList<ToggleCardItemComponent>;
 
-    cardItemsArray$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(this.cardItemsArray); // TODO: Fetch items
+    @Input("items") cardItemsArray: Array<Item>;
+
+    cardItemsArray$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>([]);
     cardItemsUpdate$: Subject<any> = new Subject<any>();
     sourceFilter$: Observable<string>;
 
     constructor(public elementRef: ElementRef) {}
 
+    ngOnInit(): void {
+        this.setupClicksSubscription();
+        this.filterLabel = `Filter ${this.label}`;
+        this.editorLabel = `${this.label} Category`;
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        // changes.prop contains the old and the new value...
+        if(changes.cardItemsArray.previousValue !== changes.cardItemsArray.currentValue){
+            this.cardItemsArray$.next(this.cardItemsArray);
+        }
+      }
+
     ngOnDestroy(): void {
         (<any>Object).values(this.subscriptions).forEach(subscription => subscription.unsubscribe());
     }
+
     ngAfterViewInit() {
         this.subscriptions.sourceFilterSubscription =
             this.filterItems &&
@@ -85,20 +63,11 @@ export class CardItemListComponent implements AfterViewInit {
                 .subscribe();
 
         this.cardItems.changes.subscribe(() => {
-            setTimeout(() => {
-                this.cardItems.forEach(si => {
-                    if (!this.sourceClicksSubscriptions[si.item.id]) {
-                        this.sourceClicksSubscriptions[si.item.id] = new BehaviorSubject(si.clicks$);
-                    } else {
-                        this.sourceClicksSubscriptions[si.item.id].next(si.clicks$);
-                    }
-                });
-                this.cardItemsUpdate$.next();
-            }, 0);
+            this.setupClicksSubscription();
         });
 
         this.cardItemsUpdate$.subscribe(() => {
-            (<any>Object).entries(this.sourceClicksSubscriptions).forEach(([key, subjectClick$]) => {
+            (<any>Object).entries(this.clicksSubscriptions).forEach(([key, subjectClick$]) => {
                 if (subjectClick$.observers.length === 0) {
                     this.subscriptions[`${key}`] = subjectClick$.pipe(switchAll()).subscribe(newItem => {
                         console.log(`source Item clicked:`, newItem);
@@ -116,6 +85,18 @@ export class CardItemListComponent implements AfterViewInit {
         });
     }
 
+    setupClicksSubscription(){
+        setTimeout(() => {
+            this.cardItems.forEach(si => {
+                if (!this.clicksSubscriptions[si.item.id]) {
+                    this.clicksSubscriptions[si.item.id] = new BehaviorSubject(si.clicks$);
+                } else {
+                    this.clicksSubscriptions[si.item.id].next(si.clicks$);
+                }
+            });
+            this.cardItemsUpdate$.next();
+        }, 0);
+    }
     onAddItem(data) {
         if (!data) return;
 
